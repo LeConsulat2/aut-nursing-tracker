8588ae514ae8639d0b156073dbe0b2a4e41a1fb0
LeConsulat2 <wooine2@gmail.com>
2025-06-15 12:18:34 +1200

    Updates

--- src/constants/grades.ts
+++
@@ -1,3 +0,0 @@
-// src/constants/grades.ts
-export const PASSING_GRADES = ['A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'P'];
-export const FAILING_GRADES = ['D', 'D+', 'E', 'F', 'D-', 'E+', 'E-', 'F+', 'F-', 'U'];
